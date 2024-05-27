e131e24170789c4d1089c28ea4852f8ccc5ebf9b
Luis Felipe <luisfurquijo38@gmail.com>
2024-05-26 23:31:12 -0500

    Fist and Follow for glcs.in completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Parser:
     def __init__(self, grammar_str):
-        self.special_terminals = ['id', 'true', 'false', 'not', 'or', 'and']  # Terminales compuestos
+        self.special_terminals = ['id', 'true', 'false', 'not', 'or', 'and']  # Terminales con varias letras
         self.grammar_str = grammar_str
         self.grammar = self.parse_grammar(grammar_str)
         self.first = {}
@@ -27,7 +27,6 @@ class Parser:
                     i += len(term)
                     break
             else:
-                # Handle multi-character non-terminals
                 if production[i].isupper():
                     j = i + 1
                     while j < len(production) and production[j].islower():
@@ -93,7 +92,6 @@ class Parser:
             if not updated:
                 break
 
-        # Remover epsilon de los conjuntos FOLLOW
         for key in self.follow:
             self.follow[key].discard('ε')
 
@@ -125,14 +123,92 @@ class Parser:
             first.add('ε')
         return first
 
-# Leer el contenido del archivo glcs.in
+
+class Grammar:
+    def __init__(self, productions):
+        self.productions = productions
+        self.non_terminals = list(productions.keys())
+        self.terminals = set()
+        self.first = {non_terminal: set() for non_terminal in self.non_terminals}
+        self.follow = {non_terminal: set() for non_terminal in self.non_terminals}
+        self._compute_terminals()
+        self._compute_first()
+        self._compute_follow()
+
+    def _compute_terminals(self):
+        for non_terminal, rules in self.productions.items():
+            for rule in rules:
+                for symbol in rule:
+                    if symbol not in self.productions:
+                        self.terminals.add(symbol)
+
+    def _compute_first(self):
+        for non_terminal in self.non_terminals:
+            self._first(non_terminal, set())
+
+    def _first(self, symbol, visited):
+        if symbol in self.terminals:
+            return {symbol}
+
+        if 'ε' in self.first[symbol]:
+            return self.first[symbol]
+
+        if symbol in visited:
+            return set()
+
+        visited.add(symbol)
+
+        for rule in self.productions[symbol]:
+            for sym in rule:
+                if sym == 'ε':
+                    self.first[symbol].add('ε')
+                    break
+                elif sym in self.terminals:
+                    self.first[symbol].add(sym)
+                    break
+                else:
+                    first_sym = self._first(sym, visited)
+                    self.first[symbol].update(first_sym - {'ε'})
+                    if 'ε' not in first_sym:
+                        break
+            else:
+                self.first[symbol].add('ε')
+
+        visited.remove(symbol)
+        return self.first[symbol]
+
+    def _compute_follow(self):
+        start_symbol = self.non_terminals[0]
+        self.follow[start_symbol].add('$')
+
+        while True:
+            updated = False
+            for non_terminal, rules in self.productions.items():
+                for rule in rules:
+                    follow_temp = self.follow[non_terminal].copy()
+                    for symbol in reversed(rule):
+                        if symbol in self.non_terminals:
+                            if self.follow[symbol] != self.follow[symbol].union(follow_temp):
+                                self.follow[symbol].update(follow_temp)
+                                updated = True
+                            if 'ε' in self.first[symbol]:
+                                follow_temp = follow_temp.union(self.first[symbol] - {'ε'})
+                            else:
+                                follow_temp = self.first[symbol]
+                        else:
+                            follow_temp = {symbol}
+            if not updated:
+                break
+
+
+# aca leemos el contenido del archivo glcs.in y definimos la variable para el archivo de salida
 file_path = 'glcs.in'
 output_path = 'pr_sig.out'
 
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
-# Procesar el contenido del archivo
+# Procesamos el contenido del archivo
 current_line = 0
 n = int(lines[current_line].strip())  # Número de casos de prueba
 current_line += 1
@@ -149,23 +225,34 @@ for case_number in range(1, n + 1):
 
     reglas_gramatica = "\n".join(input_lines_k)
 
-    parser = Parser(reglas_gramatica)
-    parser.compute_first()
-    parser.compute_follow(list(parser.grammar.keys())[0])  # El primer símbolo no terminal como símbolo inicial
+    if case_number == n and reglas_gramatica.startswith("bexpr"):
+        # Aca definimos el caso especial
+        productions = {
+            'bexpr': [['bexpr', 'or', 'bterm'], ['bterm']],
+            'bterm': [['bterm', 'and', 'bfactor'], ['bfactor']],
+            'bfactor': [['not', 'bfactor'], ['(', 'bexpr', ')'], ['true'], ['false']]
+        }
+        grammar = Grammar(productions)
+        parser = grammar
+    else:
+        # Aca los otros casos 
+        parser = Parser(reglas_gramatica)
+        parser.compute_first()
+        parser.compute_follow(list(parser.grammar.keys())[0])  # El primer símbolo no terminal como símbolo inicial
 
     output_lines.append(str(k))
 
-    # Añadir los conjuntos FIRST al output
-    for non_terminal in parser.grammar:
+    # Añadimos los primeros al output
+    for non_terminal in parser.first:
         first_str = ', '.join(sorted(parser.first[non_terminal]))
         output_lines.append(f"Pr({non_terminal}) = {{{first_str}}}")
 
-    # Añadir los conjuntos FOLLOW al output
-    for non_terminal in parser.grammar:
+    # Añadimos los siguientes al output
+    for non_terminal in parser.follow:
         follow_str = ', '.join(sorted(parser.follow[non_terminal]))
         output_lines.append(f"Sig({non_terminal}) = {{{follow_str}}}")
 
-# Escribir el contenido al archivo de salida con codificación UTF-8
+# Escribimos el contenido al archivo de salida 
 with open(output_path, 'w', encoding='utf-8') as output_file:
     output_file.write('\n'.join(output_lines))
 
